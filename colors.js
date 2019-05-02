1c44dc9204c278ab598feaab73ba2960089022e8
web2 <yjk2149@naver.com>
2019-05-02 13:04:01 +0900

    JS 파일로 쪼개기

---
+++ colors.js
@@ -0,0 +1,35 @@
+var Links ={
+  setColor:function(color){
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i<alist.length){
+    console.log(alist[i]);
+    alist[i].style.color = color;
+    i = i+1;
+    }
+  }
+}
+var Body ={
+  setColor:function(color){
+    document.querySelector('body').style.color = color;
+  },
+  setBackgroundColor:function(color){
+    document.querySelector('body').style.backgroundColor = color;
+  }
+}
+function nightDayHandler(my){
+  var target=document.querySelector('body');
+  if(my.value === 'Night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    my.value = 'Day';
+
+    Links.setColor('powderblue');
+  } else {
+    Body.setBackgroundColor('#FFF9E1');
+    Body.setColor('black');
+    my.value = 'Night';
+
+    Links.setColor('blue');
+  }
+}
